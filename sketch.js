f138e77229da31caff1692d77f549cbe04883a71
olibraun <oliverbraun89@gmail.com>
2017-10-10 19:32:25 +0200

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,16 @@
+//Sketch-Datei für Rullo
+function setup() {
+  createCanvas(500,700);
+}
+
+function draw() {
+  background(51);
+}
+
+function mousePressed(){
+  noCursor();
+}
+
+function mouseReleased(){
+  cursor();
+}
