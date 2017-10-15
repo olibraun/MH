dee67f23defcfc2d3314ad23600cc48eefc54670
olibraun <oliverbraun89@gmail.com>
2017-10-15 11:00:58 +0200

    Loaders

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
 //Sketch-Datei für Moorhuhn
-
 let debug = false;
-let manager = new gameManager();
+let manager;
 
 // Datenmaterial
 let img_background;
@@ -9,8 +8,11 @@ let img_chicken_alive;
 let img_chicken_dead;
 let img_chicken_alive_2;
 let img_chicken_dead_2;
+let img_chicken_huge;
 let img_bullet;
+
 let snd_background;
+let snd_song;
 let snd_big_chicken_appears;
 let snd_big_chicken_shot;
 let snd_chicken_hit_close;
@@ -24,28 +26,13 @@ let snd_gun_empty;
 let chickens = [];
 
 function preload(){
-  //Load images
-  img_background = loadImage("material/meadow.jpg");
-  img_chicken_alive = loadImage("material/chicken1.png");
-  img_chicken_alive_2 = loadImage("material/chicken2.png");
-  img_chicken_dead = loadImage("material/chickenDead.png");
-  img_chicken_dead_2 = loadImage("material/chickenDead2.png");
-  img_bullet = loadImage("material/bullet.png");
-
-  //Load sounds
-  snd_background = loadSound("material/sfx_background_birds_STEREO_LOOP.wav");
-  snd_big_chicken_appears = loadSound("material/sfx_big_chicken_pops_up.wav");
-  snd_big_chicken_shot = loadSound("material/sfx_big_chicken_shot.wav");
-  snd_chicken_hit_close = loadSound("material/sfx_chicken_shot_CLOSE.wav");
-  snd_chicken_hit_far = loadSound("material/sfx_chicken_shot_FAR.wav");
-  snd_chicken_hit_mid = loadSound("material/sfx_chicken_shot_MID.wav");
-  snd_gun_reload = loadSound("material/sfx_gun_reload.wav");
-  snd_gun_fire = loadSound("material/sfx_shotgun_blast.wav");
-  snd_gun_empty = loadSound("material/sfx_typename_click.wav");
+  loadImages();
+  loadSounds();
 }
 
 function setup() {
   createCanvas(1200,700);
+  manager = new gameManager();
   masterVolume(0);
 }
 
