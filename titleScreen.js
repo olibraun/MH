d843d23327c7370c728848cdc01e2b42f74a7e39
olibraun <oliverbraun89@gmail.com>
2017-10-15 11:32:59 +0200

    Adjustments to title screen

--- titleScreen.js
+++ titleScreen.js
@@ -3,6 +3,10 @@ class titleScreen{
   }
 
   show(){
+    background(51);
+    let bbb = img_chicken_huge;
+    image(bbb,0,height-bbb.height);
+    image(bbb,width-bbb.width,height-bbb.height);
     fill(0, 255, 0);
     noStroke();
     textAlign(CENTER,CENTER);
