9cb3d3c2d8b916e69ddca10a449ff580e323fffc
olibraun <oliverbraun89@gmail.com>
2017-10-17 22:17:52 +0200

    Big chicken appears, doesnt disappear yet...

--- scripts/classes/manager.js
+++ scripts/classes/manager.js
@@ -12,6 +12,7 @@ class gameManager{
     this.backLayer = [];
     this.middleLayer = [];
     this.frontLayer = [];
+    this.hugeLayer = [];
     this.scoreDisplays = [];
 
     //Call updateTimer every 1000 milliseconds automatically
@@ -24,6 +25,7 @@ class gameManager{
     this.backLayer = [];
     this.middleLayer = [];
     this.frontLayer = [];
+    this.hugeLayer = [];
     this.scoreDisplays = [];
     this.gun.reload();
     this.screenState="PLAYING";
@@ -56,6 +58,10 @@ class gameManager{
             break;
         }
       }
+      //Generate huge chickens
+      if(random(1)<0.001 && this.hugeLayer.length == 0){
+        this.hugeLayer.push(new huge_chicken());
+      }
 
       //Update and, if necessary, remove chickens
       for(let i=this.backLayer.length-1; i >= 0; i--){
@@ -74,6 +80,12 @@ class gameManager{
         this.frontLayer[i].update();
         if(this.frontLayer[i].offScreen()){
           this.frontLayer.splice(i,1);
+        }
+      }
+      for(let i=this.hugeLayer.length-1; i >= 0; i--){
+        this.hugeLayer[i].update();
+        if(this.hugeLayer[i].offScreen()){
+          this.hugeLayer.splice(i,1);
         }
       }
       //If appropriate, remove score displays
@@ -100,7 +112,17 @@ class gameManager{
         if(gun_result){
           //Make sure we only kill one chicken per click
           let chicken_killed = false;
-          //Kill the front chickens first
+          //Kill the front chickens first -- after the huge ghicken
+          for(let i=this.hugeLayer.length-1; i >= 0; i--){
+            if(this.hugeLayer[i].hits(mouseX,mouseY) && !chicken_killed && this.hugeLayer[i].alive){
+              this.hugeLayer[i].alive = false;
+              snd_big_chicken_shot.play();
+              chicken_killed = true;
+              this.scoreDisplays.push(new scoreDisplay("30",x,y) );
+              this.score += 30;
+              break;
+            }
+          }
           for(let i=this.frontLayer.length-1; i >= 0; i--){
             if(this.frontLayer[i].hits(mouseX,mouseY) && !chicken_killed && this.frontLayer[i].alive){
               this.frontLayer[i].alive = false;
@@ -171,6 +193,9 @@ class gameManager{
         }
         for(let i=this.frontLayer.length-1; i >= 0; i--){
           this.frontLayer[i].show();
+        }
+        for(let i=this.hugeLayer.length-1; i >= 0; i--){
+          this.hugeLayer[i].show();
         }
         for(let i=this.scoreDisplays.length-1; i >= 0; i--){
           this.scoreDisplays[i].updateAndShow();
--- scripts/sketch.js
+++ scripts/sketch.js
@@ -37,7 +37,6 @@ function preload(){
 function setup() {
   createCanvas(1200,700);
   manager = new gameManager();
-  masterVolume(0);
 }
 
 function draw() {
