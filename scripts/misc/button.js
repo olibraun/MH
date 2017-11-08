124f440eacdf886ec02297976af2a62f905247b1
olibraun <oliverbraun89@gmail.com>
2017-11-08 22:50:37 +0100

    Rewrite button into class

--- scripts/misc/button.js
+++ scripts/misc/button.js
@@ -1,16 +1,18 @@
-myButton = function(x,y,msg){
-  this.w = 100;
-  this.h = 50;
-  this.pos = createVector(x,y);
-  this.msg = msg;
+class myButton{
+  constructor(x,y,msg){
+    this.w = 100;
+    this.h = 50;
+    this.pos = createVector(x,y);
+    this.msg = msg;
+  }
 
-  this.hits = function(x,y){
+  hits(x,y){
     let X = this.pos.x;
     let Y = this.pos.y;
     return (abs(X-x) <= this.w/2 && abs(Y-y) <= this.h/2);
   }
 
-  this.show = function(){
+  show(){
     stroke(179, 179, 204);
     strokeWeight(3);
     noFill();
