5e906b7916f2559892bf9bcbe0d301c675fb446a
olibraun <oliverbraun89@gmail.com>
2017-12-16 08:55:00 +0100

    Make the huge chicken disappear automatically after a certain amount of time.

--- scripts/classes/hugechicken.js
+++ scripts/classes/hugechicken.js
@@ -3,13 +3,19 @@ class huge_chicken{
     this.img = img_chicken_huge;
     this.pos = createVector(random(0,width-this.img.width),height-this.img.height);
     this.alive = true;
+    this.alive_time = 60;
 
     snd_big_chicken_appears.play();
   }
 
   update(){
-    if(!this.alive){
+    if(this.alive){
+      this.alive_time--;
+    }else{
       this.pos.y+=10;
+    }
+    if(this.alive_time < 0 && this.alive){
+      this.alive = false;
     }
   }
 
