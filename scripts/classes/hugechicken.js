9c3f218066f796ec71805115d43d20f9a21fa830
olibraun <oliverbraun89@gmail.com>
2018-02-08 18:02:12 +0100

    feat: Improve huge chicken behavior when moving off screen.

--- scripts/classes/hugechicken.js
+++ scripts/classes/hugechicken.js
@@ -1,5 +1,5 @@
 class huge_chicken{
-  constructor(){
+  constructor() {
     this.img = img_chicken_huge;
     this.pos = createVector(random(0,width-this.img.width),height-this.img.height);
     this.alive = true;
@@ -9,25 +9,30 @@ class huge_chicken{
   }
 
   update(){
-    if(this.alive){
+    // Decrease alive_time as long as the chicken is alive
+    if(this.alive) {
       this.alive_time--;
-    }else{
-      this.pos.y+=10;
     }
-    if(this.alive_time < 0 && this.alive){
+
+    if(this.alive_time < 0) {
+      this.pos.y += 10;
+    }
+
+    // Kill chicken when it is off the screen
+    if(this.offScreen() && this.alive) {
       this.alive = false;
     }
   }
 
-  offScreen(){
+  offScreen() {
     return this.pos.y >= height;
   }
 
-  hits(x,y){
+  hits(x,y) {
     return x >= this.pos.x && x <= this.pos.x + this.img.width && y >= this.pos.y && y <= this.pos.y + this.img.height;
   }
 
-  show(){
+  show() {
     image(this.img,this.pos.x,this.pos.y);
   }
 }
