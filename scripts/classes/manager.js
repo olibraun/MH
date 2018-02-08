72a9947f863828c315fc92c50ccd9b2cf3a5b466
olibraun <oliverbraun89@gmail.com>
2018-02-08 18:12:49 +0100

    feat: Even more chickens!!!!

--- scripts/classes/manager.js
+++ scripts/classes/manager.js
@@ -41,7 +41,7 @@ class gameManager{
     }
     if(this.screenState === "PLAYING"){
       //Generate new chickens
-      if(random(1) < 0.01){
+      if(random(1) < 0.02){
         let temp_kind = random(["FRONT","MIDDLE","BACK"]);
         let temp_chicken = new Chicken( temp_kind , random(["RIGHT_TO_LEFT","LEFT_TO_RIGHT"]) );
         switch(temp_kind){
@@ -59,7 +59,7 @@ class gameManager{
         }
       }
       //Generate huge chickens
-      if(random(1)<0.0015 && this.hugeLayer.length == 0){
+      if(random(1)<0.002 && this.hugeLayer.length == 0){
         this.hugeLayer.push(new huge_chicken());
       }
 
